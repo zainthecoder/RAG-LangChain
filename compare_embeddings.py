5291cce55cf83550fcfd3c32804a2a6fee2e0245
s28zabed <s28zabed@uni-bonn.de>
2024-03-04 15:22:15 +0100

    RAG implementation

---
+++ compare_embeddings.py
@@ -0,0 +1,27 @@
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.evaluation import load_evaluator
+from creds import creds
+
+import os
+os.environ['OPENAI_API_KEY'] = creds['api_key']
+
+
+def main():
+    # Get embedding for a word.
+    embedding_function = OpenAIEmbeddings()
+    vector = embedding_function.embed_query("apple")
+    print(f"Vector for 'apple': {vector}")
+    print(f"Vector length: {len(vector)}")
+
+    # Compare vector of two words
+    evaluator = load_evaluator("pairwise_embedding_distance")
+    words = ("apple", "iphone")
+    x = evaluator.evaluate_string_pairs(prediction=words[0], prediction_b=words[1])
+    print(f"Comparing ({words[0]}, {words[1]}): {x}")
+    words = ("orange", "iphone")
+    x = evaluator.evaluate_string_pairs(prediction=words[0], prediction_b=words[1])
+    print(f"Comparing ({words[0]}, {words[1]}): {x}")
+
+
+if __name__ == "__main__":
+    main()
